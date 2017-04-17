e40b01b5006ad44841b1f87a00ac4c653322eb20
rootnaim <root.naim@gmail.com>
2017-04-18 04:29:05 +0600

    result-ver-1.00

---
+++ index.php
@@ -0,0 +1,187 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>CGPA Calculator</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/css.css" rel="stylesheet">
+
+  <link href="https://fonts.googleapis.com/css?family=Sansita" rel="stylesheet">
+
+    <!-- Bootstrap -->
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+
+
+    <div class="container-fluid ">
+      <div class="row"><img class='img-responsive' src="img/contact.jpg"></div>
+    </div>
+
+
+    <div class="container">
+
+      <div class="row">
+        
+        <div class="col-md-3"style="margin-top:60px">
+          
+          <div class="panel panel-success">
+            <div class="panel-heading" style="font-size:15px;text-align:center">UGC Uniform Grading System</div>
+            <div class="panel-body no">
+              <img class='img-responsive' src="img/ugc.jpg">
+              
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md-6"style="border-raduis:20px;margin-top:-110px;background:white;padding-top:20px;padding-bottom:20px">
+            <div class="panel panel-success">
+              <div class="panel-heading" style="font-size:15px;text-align:center;font-size:30px;">Generate your result</div>
+                <div class="panel-body no" style="margin-bottom:20px;margin-top:20px;">
+                    <div class="col-md-6 col-sm-6 col-xs-6">
+                        <div class="input-group p" >
+                          <span class="input-group-addon" style="background:#FFFF00;color:black">1st</span>
+                          <input type="text" class="form-control" placeholder="Credits" id="c1">
+                          <input type="text" class="form-control" placeholder="CGPA"id="g1">
+                        </div>
+
+                        <div class="input-group p">
+                          
+                          <input type="text" class="form-control" placeholder="Credits" aria-describedby="basic-addon1"id="c2">
+                          <input type="text" class="form-control" placeholder="CGPA" aria-describedby="basic-addon1"id="g2">
+                          <span class="input-group-addon"style="background:#9933FF;color:black" id="basic-addon1">2nd</span>
+                        </div>
+                        
+                        <div class="input-group p" >
+                          <span class="input-group-addon"style="background:#FFC000;color:black" >3rd</span>
+                          <input type="text" class="form-control" placeholder="Credits"id="c3">
+                          <input type="text" class="form-control" placeholder="CGPA"id="g3">
+                        </div>
+
+                        <div class="input-group p">
+                          
+                          <input type="text" class="form-control" placeholder="Credits" aria-describedby="basic-addon1"id="c4">
+                          <input type="text" class="form-control" placeholder="CGPA" aria-describedby="basic-addon1"id="g4">
+                          <span class="input-group-addon" style="background:#3333CC;color:black" id="basic-addon1">4th</span>
+                        </div>
+
+                        <div class="input-group p" >
+                          <span class="input-group-addon"style="background:#FF9900;color:black" >5th</span>
+                          <input type="text" class="form-control" placeholder="Credits"id="c5">
+                          <input type="text" class="form-control" placeholder="CGPA"id="g5">
+                        </div>
+
+                        <div class="input-group p">
+                          
+                          <input type="text" class="form-control" placeholder="Credits" aria-describedby="basic-addon1"id="c6">
+                          <input type="text" class="form-control" placeholder="CGPA" aria-describedby="basic-addon1"id="g6">
+                          <span class="input-group-addon" id="basic-addon1"style="background:#3366FF;color:black">6th</span>
+                        </div> 
+                    </div>
+
+                    <div class="col-md-6 col-sm-6 col-xs-6">
+                        <div class="input-group p" >
+                          <span class="input-group-addon"style="background:#4BACC6;color:black;" >7th</span>
+                          <input type="text" class="form-control" placeholder="Credits"id="c7">
+                          <input type="text" class="form-control" placeholder="CGPA"id="g7">
+                        </div>
+
+                        <div class="input-group p">
+                          
+                          <input type="text" class="form-control" placeholder="Credits" aria-describedby="basic-addon1"id="c8">
+                          <input type="text" class="form-control" placeholder="CGPA" aria-describedby="basic-addon1"id="g8">
+                          <span class="input-group-addon" id="basic-addon1"style="background:#FF6600;color:black">8th</span>
+                        </div>
+                        
+                        <div class="input-group p" >
+                          <span class="input-group-addon"style="background:#037D43;color:black">9th</span>
+                          <input type="text" class="form-control" placeholder="Credits"id="c9">
+                          <input type="text" class="form-control" placeholder="CGPA"id="g9">
+                        </div>
+
+                        <div class="input-group p">
+                          
+                          <input type="text" class="form-control" placeholder="Credits" aria-describedby="basic-addon1"id="c10">
+                          <input type="text" class="form-control" placeholder="CGPA" aria-describedby="basic-addon1"id="g10">
+                          <span class="input-group-addon" id="basic-addon1"style="background:#FF0000;color:black">10th</span>
+                        </div>
+
+                        <div class="input-group p" >
+                          <span class="input-group-addon"style="background:#80D927;color:black">11th</span>
+                          <input type="text" class="form-control" placeholder="Credits"id="c11">
+                          <input type="text" class="form-control" placeholder="CGPA"id="g11">
+                        </div>
+
+                        <div class="input-group p">
+                          
+                          <input type="text" class="form-control" placeholder="Credits" aria-describedby="basic-addon1"id="c12">
+                          <input type="text" class="form-control" placeholder="CGPA" aria-describedby="basic-addon1"id="g12">
+                          <span class="input-group-addon" id="basic-addon1"style="background:#CC0066;color:black">12th</span>
+                        </div> 
+
+                    </div>       
+
+                </div>
+                <div class="panel-footer" style="font-size:30px;min-height:60px;" >
+                  <div class="col-md-6 col-sm-6 col-xs-6">
+                      <input type="text" class="form-control"placeholder="Insert Student ID "style="text-align:center"id="student-id">
+                  </div>
+                  <div class="col-md-6 col-sm-6 col-xs-6">
+                      <button type="button" class="btn btn-block btn-info">Check C.G.P.A !!</button>
+                  </div>
+
+                </div>
+
+              </div>
+
+        </div>
+
+        <div class="col-md-3 col-sm-12 col-xs-12" style="margin-top:60px;text-align:center;font-size:20px;">
+
+          <div class="panel panel-success">
+            <div class="panel-heading" style="font-size:30px;">Your Result</div>
+            <div class="panel-body">
+              <div class="alert alert-info" role="alert" style="margin-top:20px;">
+                <a  class="alert-link">CGPA 4.00</a>
+              </div>
+              <div class="alert alert-warning" role="alert">
+                <a  class="alert-link">Grade: A+</a>
+              </div>
+            </div>
+            <div class="panel-footer" style="font-size:30px;">Successful</div>
+          </div>
+
+
+        </div>
+        
+
+      </div>         
+    </div>
+
+
+    <div class="container-fluid">
+      <div class="row">
+        <div class="footer "style="font-size:20px">
+          <label style="margin-top:10px">Created by - </label><a style="font-size:20px"href="www.yescoders.com">Yescoders</a>
+        </div>
+      </div>
+    </div>
+
+  </body>
+
+
+    <script src="js/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+</html>
